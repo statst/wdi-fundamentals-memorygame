d6076f4c7b46f14c2cdb18ae0d6b976ffee8dbee
jyoti <jyoti@jyotis-MacBook-Pro-2.local>
2020-03-16 19:08:29 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -8,19 +8,39 @@
 //alert('Hello, friends.');
 const cards= ["queen", "queen", "king", "king"];
 const cardsInPlay=[];
-const cardOne= cards[0];
-cardsInPlay.push('cardOne');
-const cardTwo= cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-if (cardsInPlay.length===2){
-	if (cardsInPlay[0]===cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
-}
-//function flipCard(cardId){
-
+cardsInPlay.push('cardsID');
+//const cardOne= cards[0];
+//cardsInPlay.push('cardOne');
+//const cardTwo= cards[2];
+//cardsInPlay.push('cardTwo');
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardTwo);
+//if (cardsInPlay.length===2){
+	//if (cardsInPlay[0]===cardsInPlay[1]) {
+	//	alert("You found a match!");
+	//} else {
+		//alert("Sorry, try again.");
+	//}
 //}
+function checkForMatch(){
+ if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+function flipCard(cardId){
+//f (cardsInPlay.length===2){
+	//if (cardsInPlay[0]===cardsInPlay[1]) {
+		//alert("You found a match!");
+	//} else {
+		//alert("Sorry, try again.");
+	//}
+	//checkForMatch();
+//}
+
+console.log("User flipped " + cards[cardId]);
+checkForMatch();
+}
+flipCard(0);
+flipCard(2);
