1f22247fd06b7f9955b9434297ea726b7295a889
jyoti <jyoti@jyotis-MacBook-Pro-2.local>
2020-03-16 18:22:28 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,26 @@
-console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+//console.log("Up and running!");
+//let cardOne = "queen";
+//let cardTwo = "queen";
+//let cardThree = "king";
+//let cardFour = "king";
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardFour);
+//alert('Hello, friends.');
+const cards= ["queen", "queen", "king", "king"];
+const cardsInPlay=[];
+const cardOne= cards[0];
+cardsInPlay.push('cardOne');
+const cardTwo= cards[2];
+cardsInPlay.push('cardTwo');
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
+if (cardsInPlay.length===2){
+	if (cardsInPlay[0]===cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+//function flipCard(cardId){
+
+//}
