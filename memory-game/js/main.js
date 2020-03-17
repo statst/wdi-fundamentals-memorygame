977a908db3191afcc4469307f266bfb30116dfd7
jyoti <jyoti@jyotis-MacBook-Pro-2.local>
2020-03-16 23:28:57 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -24,17 +24,33 @@ cardImage: 'images/king-of-diamonds.png'
 const cardsInPlay=[];
 cardsInPlay.push('cardsID');
 
+
+
 function checkForMatch(){
+	cardElement.setAttribute('src', "images/back.png");
  if (cardsInPlay[0] === cardsInPlay[1]) {
   alert("You found a match!");
 } else {
  alert("Sorry, try again.");
 }
+console.log(cards[cardId].cardImage);
 }
-function flipCard(cardId){
+function flipCard(){
+	this.getAttribute('attribute-we-want-to-get-goes-here');
 console.log("User flipped " + cards[cardId].rank);
 console.log("User flipped " + cards[cardId].cardImage);
 checkForMatch();
 }
-flipCard(0);
-flipCard(2);
+function createBoard(){
+	for (let i = 0; i < cards.length; i++) {
+    let cardElement = document.createElement('img');
+     cardElement.setAttribute('src', 'images/back.png');
+     cardElement.setAttribute('rank', i);
+     cardElement.addEventListener("click", flipCard);
+     //document.getElementsByTagName('id').appendChild(cardElement);
+     //cardElement.textContent='game-board';
+     //cardElement.appendChild(".game-board");
+     //document.getElementsByTagName('id')[0].appendChild(cardElement);
+}
+}
+createBoard();
